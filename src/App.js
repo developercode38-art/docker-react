15d1471235d46298e535708c1dc7a89e6e99af95
developer 38 <developer.code38@gmail.com>
2026-02-19 00:34:45 -0500

    version 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Descipline
+         Descipline MATTERS
         </p>
         <a
           className="App-link"
